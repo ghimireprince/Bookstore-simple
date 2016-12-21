5418b1351907991ef1342b60a8dc9a0dae21c028
kamalpandey2013 <kamal.pandey2013@gmail.com>
2016-12-21 15:42:00 +0530

    little javascript demo

---
+++ client/scripts/app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by kamal on 12/21/16.
+ */
+window.onload = function () {
+    var addToCartButton = document.getElementById('addToCart');
+    addToCartButton.onclick = function () {
+        alert('Item successfully added to cart');
+    }
+}
